2d38fb5d5057518b0ca52cf710e02e7ffee3c466
Sanket Chauhan <djangotestmail2021@gmail.com>
2021-11-30 18:04:56 +0530

    Day3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_testapp/home.dart';
+import 'package:flutter_testapp/screens/home.dart';
+import 'package:flutter_testapp/screens/login.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     //int cn = 30;
-    return HomePage();
+    return MaterialApp(
+      //home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      routes: {
+        "/": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+    );
   }
 }
